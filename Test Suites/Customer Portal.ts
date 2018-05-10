52f45421c2410e335755472637a1473721e108b6
BRIDGE-SERVER\Neethu.v <neethu.v@bridge-india.in>
2018-05-10 18:32:17 +0530

    Save configuration

--- Test Suites/Customer Portal.ts
+++ Test Suites/Customer Portal.ts
@@ -4,7 +4,7 @@
    <name>Customer Portal</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-05-10T11:04:22</lastRun>
+   <lastRun>2018-05-10T18:28:42</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
@@ -26,73 +26,73 @@
    <testCaseLink>
       <guid>1a36d77f-f2f5-4175-8363-5068be42a655</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Forgot Password</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>98a1a103-af9e-4394-aa87-000131106a0e</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Customer Registration Button</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>c490dabc-e99e-406b-ad69-e317fb1037a9</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Customer Registration Form</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>5f3771fe-5273-43b9-88c2-b3c9905a8751</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Customer-Login</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>9231f260-d3ed-4d0c-ac2e-05bd1322e8dc</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Move to Start Page</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>899e8c15-aa59-496f-ad77-47265ac90fa5</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Municipality dropdown field-start page</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>436dc18e-14a6-4c29-b628-24a0a1e72f06</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Select Municiplality from Start page</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>9a6e1998-517d-4824-a90d-68e981ebcc30</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Select House From House Listing Page - HB</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>2bd3d80c-137a-4471-98d3-c938bea7f2b9</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Make Changes in Configuration - Customer</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>7bbb3d0c-086d-440f-bb04-805d6fb871b2</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Save Configuration</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>464abd6f-6929-47a5-be29-249f089d8981</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Navigation to Offert Popup</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>6708f797-57c7-478c-b894-9c8dee9e1140</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Fill into Offert Form</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
